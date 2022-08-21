13871f206ac8d3f4349102ddd5f68eab84026bae
korapan muangsong <muangsong_k@silpakorn.edu>
2022-08-21 12:12:07 +0700

    s1

--- lib/main.dart
+++ lib/main.dart
@@ -12,16 +12,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Korapan Muangsong'),
     );
   }
 }
-
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
 
   final String title;
 
@@ -30,6 +28,45 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  void ctof()  {
+    //int y;
+    //int x = _control;
+    an = '_celsius = _fahrenheit';
+    setState(() {
+    });
+  }
+  var an = '';
+  dynamic _control;
+  //int guess = int.tryParse(_control);
+  void ctok() {
+    int y;
+    y = ((_control)+(273.15));
+    an = '_celsius = _kelvin';
+    setState(() {
+    });
+    an = 'hoho';
+  }
+  void ftoc() {
+    int y;
+    y = (_control-32)*(5/9) as int;
+  }
+  void ftok(){
+    int y;
+    y =(_control+459.67)*(5/9) as int;
+  }
+  void ktoc(){
+    int y;
+    y =_control-273.15 as int;
+  }
+  void ktof(){
+    //int y;
+    //y = (_control*(9/5))-459.67 as int;
+    an = 'hello';
+    print (an);
+    setState(() {
+
+    });
+  }
   int _counter = 0;
 
   void _incrementCounter() {
@@ -37,7 +74,6 @@ class _MyHomePageState extends State<MyHomePage> {
       _counter++;
     });
   }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,20 +85,41 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Temperature Converter',
+              style: TextStyle(fontSize: 35),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            TextField(
+              controller: _control, //ตัวรับค่
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), hintText: 'Enter a Number' ),
             ),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+            ElevatedButton(onPressed: ctof, child: Text('celsius to fahrenheit')),
+            ElevatedButton(onPressed: ctok, child: Text('celsius to kelvin')),
+        ]
+    ),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+            ElevatedButton(onPressed: ftoc, child: Text('fahrenheit to celsius')),
+            ElevatedButton(onPressed: ftok, child: Text('fahrenheit to kelvin')),
+        ]
+            ),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+            ElevatedButton(onPressed: ktoc, child: Text('kelvin to celsius')),
+            ElevatedButton(onPressed: ktof, child: Text('kelvin to fahrenheit')),
+        ]
+            ),
+        Text(an,
+          style: TextStyle(fontSize: 30),
+        )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
